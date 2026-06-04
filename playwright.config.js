6a1a674dd14b53f094a0ce1dbaf5f7e2ff096d91
Pooja Arora (CSV) <pooja.arora@courts.vic.gov.au>
2026-06-04 14:42:39 +1000

    playwright config added

--- playwright.config.js
+++ playwright.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env'), quiet: true });
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env'), quiet: true });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -18,11 +17,19 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['json', { outputFile: 'playwright-report.json' }]
+  ],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
- use: {
-  trace: 'on',
- },
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
 
   /* Configure projects for major browsers */
   projects: [
@@ -50,24 +57,6 @@ module.exports = defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
+
